55dacfc2d9f3ae7d80b91d30063929f77e883af6
CELS0 <celsojr.300@gmail.com>
2021-12-28 13:05:55 -0300

    feat

--- src/components/Luck/Luck.tsx
+++ src/components/Luck/Luck.tsx
@@ -2,22 +2,26 @@ import { useState } from "react";
 import { Team } from "../Team/Teams";
 import './styles.scss'
 
-export function Luck(){
+export function Luck() {
     const [img1, setImg1] = useState('https://uploadnodejs.s3.amazonaws.com/3eb6cc586108e24ce0135156d8d37258-grama.jpg')
     const [img2, setImg2] = useState('https://uploadnodejs.s3.amazonaws.com/3eb6cc586108e24ce0135156d8d37258-grama.jpg')
     const [img3, setImg3] = useState('https://uploadnodejs.s3.amazonaws.com/3eb6cc586108e24ce0135156d8d37258-grama.jpg')
     const [img4, setImg4] = useState('https://uploadnodejs.s3.amazonaws.com/3eb6cc586108e24ce0135156d8d37258-grama.jpg')
-    return(
+    return (
         <div className="container-luck">
-            <h1>SORTEIO CAMPEONATO DOS FRANÇAS</h1>
-            <h2>Jogadores do Pote</h2>
-            <div>
-            <img src={img1} />
-                <img src={img2} />
-                <img src={img3} />
-                <img src={img4} />
+            <img src="https://uploadnodejs.s3.amazonaws.com/a7ac5fcf44c1a2f79eba08652301fcdc-maxresdefault.jpg" />
+            <div className="content-luck">
+                <h1>SORTEIO CAMPEONATO DOS FRANÇAS</h1>
+                <h2>Jogadores do Pote</h2>
+                <div>
+                    <img src={img1} />
+                    <img src={img2} />
+                    <img src={img3} />
+                    <img src={img4} />
+                </div>
+                <button>SORTEAR</button>
             </div>
-            <button>SORTEAR</button>
+            <img src="https://uploadnodejs.s3.amazonaws.com/d4d542df290b9cfd6173708e6d7b80d8-messi_graphic.jpg" />
         </div>
     )
 }
